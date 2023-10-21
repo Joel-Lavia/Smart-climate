40e16f9a7eb57dc3f1225555af89a31c760ff183
JoelD-418 <joeldisidi8@gmail.com>
2023-10-21 02:28:46 +0100

    initialization of the algorithm which manages the data form and returns it to the mete data table

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 //1.ciblé les input des données météos
 //2.Cibler les cases qui vaut afficher les données météos
 const inputdata = document.querySelectorAll("input");
-const weatherDdataBox = document.querySelectorAll(".weatherData")
+const weatherDataBox = document.querySelectorAll(".weatherData")
+const bouttonEnvoyer = document.querySelector("button");
 //je le stock dans une variable
 for(let i =0; i<inputdata.length;i++){
     let inputdatas = inputdata[i];
-    let weatherDdataBoxs;
-    for(let i = 0;i<weatherDdataBox.length;i++){
-        weatherDdataBoxs = weatherDdataBox[i];
-        console.log(weatherDdataBoxs);
-    }
-    console.log(inputdatas);
+bouttonEnvoyer.addEventListener("click",function(event){
+event.preventDefault();
 }
+)
+}
+
+    for(let i = 0;i<weatherDataBox.length;i++){
+        let weatherDataBoxs = weatherDataBox[i]; 
+        weatherDataBoxs.addEventListener("click",function(event){
+        
+        })
+        }
