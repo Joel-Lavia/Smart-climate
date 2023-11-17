e6d801adee9cfe730763c68f51be91272ddaad07
Joel-Lavia <joeldisidi8@gmail.com>
2023-11-17 03:40:27 +0100

    integration de prevision meteo

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 //cle API
 const apiKey = 'df50276529db577511b060092bc121b0';
+
+//url de l'appel de l'API
 let url = `https://api.openweathermap.org/data/2.5/weather?q=Kinshasa&lang=fr&appid=${apiKey}&units=metric`;
 
-fetch(url).then( Response=>Response.json().then(data =>console.log(data)));
+const nomDelaVille = document.querySelector("h2");
+const previsionClimatique = document.querySelectorAll(".box");
+console.log(previsionClimatique);
+
+fetch(url).then( Response=>
+    Response.json().then(data =>
+        {
+            nomDelaVille.innerText = data.name;
+            for(let i = 0; i<previsionClimatique.length;i++){
+            previsionClimatique[0].innerText = data.weather[0].description;
+            previsionClimatique[1].innerText = data.weather[1].description;
+            }
+            console.log(data)
+           
+        
+        
+        
+        }));
 
 
