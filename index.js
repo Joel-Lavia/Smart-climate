6a6fed4302c2f4a7bc2fa2f19555d6cfacd6d0e0
Joel-Lavia <joeldisidi8@gmail.com>
2023-11-17 02:16:26 +0100

    integration de la cle Api pour obtenir les donnees meteo

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-//1.ciblé les input des données météos
-//2.Cibler les cases qui vaut afficher les données météos
-//3.cibler le boutton evoyer
-const bouttonEnvoyer = document.querySelector("button");
-    bouttonEnvoyer.addEventListener("click", function (event) {
-     event.preventDefault(); // Empêcher le formulaire de se soumettre
+//cle API
+const apiKey = 'df50276529db577511b060092bc121b0';
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Kinshasa&lang=fr&appid=${apiKey}&units=metric`;
 
-        const inputdata = document.querySelectorAll("input");//Sélection des éléments inputs
-        const weatherDataBox = document.querySelectorAll(".weatherData"); // Sélection des éléments qui ont la class "weatherData"
-//la première boucle parcours les elements inputs et deuxième boucle parcours les  éléments avec la class "weatherData"
-        for(let i = 0; i < inputdata.length; i++) {
-            const inputdatas = inputdata[i];
-            for(let j = 0; j < weatherDataBox.length; j++) {
-                const weatherDataBoxs = weatherDataBox[j];
-//condition de verification des indix des éléments inputs et class weatherData
-                if (i === j) {
-                    weatherDataBoxs.innerText = inputdatas.value;
-                }
-            }
-        }
-    });
+fetch(url).then( Response=>Response.json().then(data =>console.log(data)));
+
+
