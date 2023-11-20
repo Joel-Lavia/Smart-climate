57d49dbe082e3560110ae3403ca8bb8a670d810a
Joel-Lavia <joeldisidi8@gmail.com>
2023-11-20 16:00:13 +0100

    integration des parametre climatique

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 const apiKey = 'df50276529db577511b060092bc121b0';
 
 //url de l'appel de l'API
-let url = `https://api.openweathermap.org/data/2.5/weather?q=Kinshasa&lang=fr&appid=${apiKey}&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Kinshasa&lang=fr&appid=${apiKey}&units=metric`;
 
 const nomDelaVille = document.querySelector("h2");
-const previsionClimatique = document.querySelectorAll(".box");
+const previsionClimatique = document.querySelector(".box");
+const climaticParameters = document.querySelectorAll(".boxsetting");
+console.log(climaticParameters);
 
-function previsionweather(firstPrevision,secondPrevision){
+function previsionweather(prevision){
+previsionClimatique.innerText = prevision;
+}
+function weatherParameters(temperatur,temperaturMax,temperaturMin,windSpeed,humidit){
+    climaticParameters[0].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperatur)}°`;
+    climaticParameters[1].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperaturMax)}°`;
+    climaticParameters[2].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperaturMin)}°`;
+    climaticParameters[3].innerHTML = `<i class="fa-solid fa-wind"></i>${Math.floor(windSpeed)} KM/H`;
+    climaticParameters[4].innerHTML = `<i class="fa-solid fa-droplet"></i>${Math.floor(humidit)}%`;
 
-        previsionClimatique[0].innerText = firstPrevision
-        previsionClimatique[1].innerText = secondPrevision;  
 }
 
 
@@ -19,6 +27,11 @@ fetch(url).then( Response=>
         {
             nomDelaVille.innerText = data.name;
             previsionweather(data.weather[0].description);
+            weatherParameters(data.main.temp,data.main.temp_max,data.main.temp_min,data.wind.speed,data.main.humidity);
+            
+            
+
+        
             
             console.log(data);
            
