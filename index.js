8afc33bae69de9492e4e80e63a0a5600b8828620
Joel-Lavia <joeldisidi8@gmail.com>
2023-11-21 03:41:46 +0100

    integration de la fonctionnalite de recherche des villes

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 //cle API
 const apiKey = 'df50276529db577511b060092bc121b0';
-
-//url de l'appel de l'API
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Kinshasa&lang=fr&appid=${apiKey}&units=metric`;
-
+//selection des elements htlm
 const nomDelaVille = document.querySelector("h2");
 const previsionClimatique = document.querySelector(".box");
 const climaticParameters = document.querySelectorAll(".boxsetting");
-console.log(climaticParameters);
+const cheachVille = document.querySelector("form");
+console.log(cheachVille); 
 
 function previsionweather(prevision){
 previsionClimatique.innerText = prevision;
@@ -15,26 +13,33 @@ previsionClimatique.innerText = prevision;
 function weatherParameters(temperatur,temperaturMax,temperaturMin,windSpeed,humidit){
     climaticParameters[0].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperatur)}°`;
     climaticParameters[1].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperaturMax)}°`;
-    climaticParameters[2].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperaturMin)}°`;
+    climaticParameters[2].innerHTML = `<i class='fa-solid fa-temperature-high'</i>${Math.floor(temperaturMin)}°`;
     climaticParameters[3].innerHTML = `<i class="fa-solid fa-wind"></i>${Math.floor(windSpeed)} KM/H`;
-    climaticParameters[4].innerHTML = `<i class="fa-solid fa-droplet"></i>${Math.floor(humidit)}%`;
-
+    climaticParameters[4].innerHTML = `<i class="fa-solid fa-droplet"></i> ${Math.floor(humidit)} %`;
 }
 
+function callApi(city){
+    //url de l'appel de l'API
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=${apiKey}&units=metric`;
+    fetch(url).then( Response=>Response.json().then(data =>{
+                nomDelaVille.innerText = data.name;
+                previsionweather(data.weather[0].description);
+                weatherParameters(data.main.temp,data.main.temp_max,data.main.temp_min,data.wind.speed,data.main.humidity);
+                console.log(data);
+            }));
+}
 
-fetch(url).then( Response=>
-    Response.json().then(data =>
-        {
-            nomDelaVille.innerText = data.name;
-            previsionweather(data.weather[0].description);
-            weatherParameters(data.main.temp,data.main.temp_max,data.main.temp_min,data.wind.speed,data.main.humidity);
-            
-            
+cheachVille.addEventListener('submit',function(event){
+event.preventDefault();
+let villeInput = document.querySelector("input").value;
+callApi(villeInput);
 
-        
-            
-            console.log(data);
-           
-        }));
+// if(villeInput == " "){
+// alert(`Veuiller entrée les Noms d'une Ville`)
+// }
+
+});
+
+callApi("Kinshasa");
 
 
