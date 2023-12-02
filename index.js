bbeba317b10c4b2a8fce2b1b9421310e87dcec4f
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-02 02:22:13 +0100

    integration d'image d'etat meteo

--- index.js
+++ index.js
@@ -1,45 +1,92 @@
 //cle API
-const apiKey = 'df50276529db577511b060092bc121b0';
+const apiKey = "df50276529db577511b060092bc121b0";
 //selection des elements htlm
 const nomDelaVille = document.querySelector("h2");
-const previsionClimatique = document.querySelector(".box");
+const previsionClimatique = document.querySelectorAll(".box");
 const climaticParameters = document.querySelectorAll(".boxsetting");
 const cheachVille = document.querySelector("form");
-console.log(cheachVille); 
+const st = document.querySelector("#state");
+console.log(cheachVille);
 
-function previsionweather(prevision){
-previsionClimatique.innerText = prevision;
+function previsionweather(prevision, weatherState) {
+  previsionClimatique[0].innerHTML = `${weatherState} ${prevision}`;
 }
-function weatherParameters(temperatur,temperaturMax,temperaturMin,windSpeed,humidit){
-    climaticParameters[0].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperatur)}°`;
-    climaticParameters[1].innerHTML = `<i class='fa-solid fa-temperature-high'></i>${Math.floor(temperaturMax)}°`;
-    climaticParameters[2].innerHTML = `<i class='fa-solid fa-temperature-high'</i>${Math.floor(temperaturMin)}°`;
-    climaticParameters[3].innerHTML = `<i class="fa-solid fa-wind"></i>${Math.floor(windSpeed)} KM/H`;
-    climaticParameters[4].innerHTML = `<i class="fa-solid fa-droplet"></i> ${Math.floor(humidit)} %`;
+function weatherParameters(
+  temperatur,
+  temperaturMax,
+  temperaturMin,
+  windSpeed,
+  humidit
+) {
+  climaticParameters[0].innerHTML = `<i class='fa-solid fa-temperature-high'></i> ${Math.round(
+    temperatur
+  )}°`;
+  climaticParameters[1].innerHTML = `<i class='fa-solid fa-temperature-high'></i>max ${Math.round(
+    temperaturMax
+  )}° `;
+  climaticParameters[2].innerHTML = `<i class='fa-solid fa-temperature-high'></i> min  ${Math.round(
+    temperaturMin
+  )}°`;
+  climaticParameters[3].innerHTML = `<i class="fa-solid fa-wind"></i> vitesse du vent ${Math.round(
+    windSpeed
+  )} KM/H`;
+  climaticParameters[4].innerHTML = `<i class="fa-solid fa-droplet"></i> Humidité relative ${Math.round(
+    humidit
+  )} %`;
 }
 
-function callApi(city){
-    //url de l'appel de l'API
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=${apiKey}&units=metric`;
-    fetch(url).then( Response=>Response.json().then(data =>{
-                nomDelaVille.innerText = data.name;
-                previsionweather(data.weather[0].description);
-                weatherParameters(data.main.temp,data.main.temp_max,data.main.temp_min,data.wind.speed,data.main.humidity);
-                console.log(data);
-            }));
+function callApi(city) {
+  //url de l'appel de l'API
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&at&lon&appid=${apiKey}&units=metric`;
+  fetch(url).then((Response) =>
+    Response.json().then((data) => {
+      nomDelaVille.innerText = data.name;
+      let state = `<img src='https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png' alt="weather"/>`;
+      previsionweather(data.weather[0].description, state);
+      previsionClimatique[1].innerHTML = `<i class='fa-solid fa-temperature-high'</i>${Math.round(
+        data.main.feels_like
+      )}°`;
+      weatherParameters(
+        data.main.temp,
+        data.main.temp_max,
+        data.main.temp_min,
+        data.wind.speed,
+        data.main.humidity
+      );
+      console.log(data);
+    })
+  );
 }
 
-cheachVille.addEventListener('submit',function(event){
-event.preventDefault();
-let villeInput = document.querySelector("input").value;
-callApi(villeInput);
+cheachVille.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let villeInput = document.querySelector("input").value;
+  callApi(villeInput);
 
-// if(villeInput == " "){
-// alert(`Veuiller entrée les Noms d'une Ville`)
-// }
-
+  // if(villeInput == " "){
+  // alert(`Veuiller entrée les Noms d'une Ville`)
+  // }
 });
 
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  // let positiion  = pos.coords;
+  // console.log("Votre position actuelle est :");
+  //   console.log(`Latitude : ${positiion.latitude}`);
+  //   console.log(`Longitude : ${positiion.longitude}`);
+  //   console.log(`La précision est de ${positiion.accuracy} mètres.`);
+  // alert(callApi());
+}
+
+function error(err) {
+  console.warn(`ERREUR (${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 callApi("Kinshasa");
-
-
